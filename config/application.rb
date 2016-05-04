da77770bc73668d019148890349e7b1bc5eadbaa
ken.bean <kishiwda_kn@yahoo.co.jp>
2016-05-04 19:58:01 +0900

    update20160614-3

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,6 @@ module SampleApp
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 		# embed authentification token to remote form in order to work even when JS isnt activated
-		config.active_view.embed_authenticity_token_in_remote_forms = true
+		config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
