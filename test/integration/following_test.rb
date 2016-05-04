f7f746c76ab340915406a939045b53128bcdfdeb
ken.bean <kishiwda_kn@yahoo.co.jp>
2016-05-04 23:41:09 +0900

    update follow logic

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class FollowingTest < ActionDispatch::IntegrationTest
   def setup
 		@user = users(:michael)
+		@to_follow = users(:archer) 
 		log_in_as(@user)
 	end
 
@@ -23,4 +24,32 @@ class FollowingTest < ActionDispatch::IntegrationTest
 			assert_select "a[href= ?]", user_path(user)
 		end
 	end
+
+	test "follow button should work as standard way" do
+		assert_difference '@user.following.count', 1 do
+			post relationships_path, followed_id: @to_follow.id
+		end
+	end
+
+  test "should follow a user with Ajax" do
+    assert_difference '@user.following.count', 1 do
+      xhr :post, relationships_path, followed_id: @to_follow.id
+    end
+  end
+
+  test "should unfollow a user the standard way" do
+    @user.follow(@to_follow)
+    relationship = @user.active_relationships.find_by(followed_id: @to_follow.id)
+    assert_difference '@user.following.count', -1 do
+      delete relationship_path(relationship)
+    end
+  end
+
+  test "should unfollow a user with Ajax" do
+    @user.follow(@to_follow)
+    relationship = @user.active_relationships.find_by(followed_id: @to_follow.id)
+    assert_difference '@user.following.count', -1 do
+      xhr :delete, relationship_path(relationship)
+    end
+  end
 end
